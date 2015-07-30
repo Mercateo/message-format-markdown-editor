db62f13cbf0155133a397d8f6e9ff98bc129da2b
Robert Bastian <robert@bastian.com>
2015-07-30 18:11:08 +0200

    revised ui after feedback, added info buttons to all edits, automatic bracket closing, progress bars now on top, sticky header

--- editor.php
+++ editor.php
@@ -3,45 +3,89 @@
     [
       "id" => "basket.cluster_badge_description",
       "group" => "basket-strings",
-      "lastChanged" => "20.03.13",
       "comment" => "COLOR: [weiß|schwarz]",
       "langs" => [
-        "de" => "Ein Cluster enthält alle Artikel, die bestimmte Anforderungen erfüllen (z. B. Kopierpapier, DIN A4, 80 g/m², {COLOR}).",
-        "en" => "A Cluster contains all items that fulfil certain requirements (e. g. copy paper, DIN A4, 80 g/m², \n{COLOR, select,\n  weiß {white}\n    other {black}\n}).",
-        "fr" => "Un cluster contient tous les produits répondant à des besoins spécifiques (papier pour imprimante, format A4, 80g/m2, \n{COLOR, select,\n  weiß {blanc}\n    other {noir}\n}).",
+        "de" => [
+          "text" => "Ein Cluster enthält alle Artikel, die bestimmte Anforderungen erfüllen (z. B. Kopierpapier, DIN A4, 80 g/m², {COLOR}).",
+          "lastAuthor" => "Robert Bastian",
+          "lastChanged" => "20.03.13"
+        ],
+        "en" => [
+          "text" => "A Cluster contains all items that fulfil certain requirements (e. g. copy paper, DIN A4, 80 g/m², \n{COLOR, select,\n  weiß {white}\n    other {black}\n}).",
+          "lastAuthor" => "Robert Bastian",
+          "lastChanged" => "20.03.13"
+        ],
+        "fr" => [
+          "text" => "Un cluster contient tous les produits répondant à des besoins spécifiques (papier pour imprimante, format A4, 80g/m2, \n{COLOR, select,\n  weiß {blanc}\n    other {noir}\n}).",
+          "lastAuthor" => "Robert Bastian",
+          "lastChanged" => "20.03.13"
+        ]
       ]
     ], 
     [
       "id" => "basket.commit.general_terms_and_privacy",
       "group" => "basket-strings",
-      "lastChanged" => "21.04.13",
-      "comment" => "AGB_URL\nDSE_LINK",
+      "comment" => "AGB_URL\nDSE_LINK\nDSE_LINK\nDSE_LINK\nDSE_LINK\nDSE_LINK",
       "langs" => [
-        "de" => "Ja, ich bestätige die [AGB]({AGB_URL}) und die [Datenschutzerklärung]({DSE_LINK}) der Mercateo AG.",
-        "en" => "Yes, I confirm the [General Terms and Conditions]({AGB_URL}) and the [data protection notice]({DSE_LINK}) of Mercateo",
-        "fr" => "Oui, j'accepte les [conditions générales de vente]({AGB_URL}) et la [déclaration de confidentialité]({DSE_LINK}) de Mercateo France SAS",
+        "de" => [
+          "text" => "Ja, ich bestätige die [AGB]({AGB_URL}) und die [Datenschutzerklärung]({DSE_LINK}) der Mercateo AG.",
+          "lastAuthor" => "Robert Bastian",
+          "lastChanged" => "21.04.13"
+        ],
+        "en" => [
+          "text" => "Yes, I confirm the [General Terms and Conditions]({AGB_URL}) and the [data protection notice]({DSE_LINK}) of Mercateo",
+          "lastAuthor" => "Robert Bastian",
+          "lastChanged" => "21.04.13"
+        ],
+        "fr" => [
+          "text" => "Oui, j'accepte les [conditions générales de vente]({AGB_URL}) et la [déclaration de confidentialité]({DSE_LINK}) de Mercateo France SAS",
+          "lastAuthor" => "Robert Bastian",
+          "lastChanged" => "21.04.13"
+        ]
       ]
     ],
     [
       "id" => "placedorder.print.hint.printing_guide",
       "group" => "basket-strings",
-      "lastChanged" => "30.04.13",
       "comment" => "OS: [mac|windows]",
       "langs" => [
-        "de" => "Sie können diese Seite ausdrucken, indem Sie die Druckfunktion Ihres Browsers verwenden ({OS, select, mac {CMD} other {STRG}}+P).",
-        "en" => "You can print out this page by using the printing function of your browser ({OS, select, mac {CMD} other {STRG}}+P).",
-        "fr" => "Vous pouvez imprimer cette page en utilisant la fonction impression de votre navigateur ({OS, select, mac {CMD} other {STRG}}+P).",
+        "de" => [
+          "text" => "Sie können diese Seite ausdrucken, indem Sie die Druckfunktion Ihres Browsers verwenden ({OS, select, mac {CMD} other {STRG}}+P).",
+          "lastAuthor" => "Robert Bastian",
+          "lastChanged" => "30.04.13"
+        ],
+        "en" => [
+          "text" => "You can print out this page by using the printing function of your browser ({OS, select, mac {CMD} other {STRG}}+P).",
+          "lastAuthor" => "Robert Bastian",
+          "lastChanged" => "30.04.13"
+        ],
+        "fr" => [
+          "text" => "Vous pouvez imprimer cette page en utilisant la fonction impression de votre navigateur ({OS, select, mac {CMD} other {STRG}}+P).",
+          "lastAuthor" => "Robert Bastian",
+          "lastChanged" => "30.04.13"
+        ]
       ]
     ],
     [
       "id" => "load_productrow&shy;.changed_deliveryTime&shy;.original_deliveryTime_was",
       "group" => "basket-strings",
-      "lastChanged" => "16.05.13",
-      "comment" => " ",
+      "comment" => null,
       "langs" => [
-        "de" => "Lieferzeit&auml;nderung! Die urspr&uuml;ngliche Lieferzeit des gespeicherten Artikels war",
-        "en" => "",
-        "fr" => "",
+        "de" => [
+          "text" => "Lieferzeit&auml;nderung! Die urspr&uuml;ngliche Lieferzeit des gespeicherten Artikels war",
+          "lastAuthor" => "Robert Bastian",
+          "lastChanged" => "16.05.13"
+        ],
+        "en" => [
+          "text" => null,
+          "lastAuthor" => null,
+          "lastChanged" => null
+        ],
+        "fr" => [
+          "text" => null,
+          "lastAuthor" => null,
+          "lastChanged" => null
+        ]
       ]
     ]
 
@@ -78,21 +122,11 @@
   <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
   <link  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-  <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.0/css/bootstrap-toggle.min.css" rel="stylesheet">
-  <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.0/js/bootstrap-toggle.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <script src="bower_components/bootstrap-checkbox.min.js"></script>
   <style>
     .CodeMirror {
       height: 100%;
-    }
-    .toggle.btn {
-      margin:5px 0px;
-    }
-    .progress {
-      margin: 12px 10px;
-      height: 30px;
-    }
-    .progress-bar {
-      padding: 5px;
     }
     .well {
       margin: 20px;
@@ -117,10 +151,11 @@
     .comment {
       max-height: 90px;
       overflow-y: auto;
-      margin: 0px 0px 10px 0px;
+      margin: 8px;
       position: absolute;
       bottom: 0px;
-      width: 480px;
+      left: 0px;
+      right: 0px;
     }
     .table> tbody > tr > td {
       padding: 0px;
@@ -129,56 +164,19 @@
       table-layout: fixed;
       width: 100px;
     }
+    .progress {
+      margin: 7px;
+    }
+    .text-center > .label {
+      margin: 7px;
+    }
   </style>
 </head>
 <body>
-  <div class='container-fluid'>
-    <table class='table table-bordered'>
-      <tr class='info'>
-        <th></th>
-        <? foreach($entries[0]['langs'] as $locale => $_): ?>
-          <th class='text-center'><?=strtoupper($locale)?></th>
-        <? endforeach; ?>
-      </tr>
-      <? foreach($entries as $index => $current): ?>
-        <tr class='<?=$index?>'>
-          <td>
-            <div class='limit'>
-              <h4 style='overflow:hidden;text-overflow: ellipsis;' name='<?=$current["id"]?>'><?=$current['id']?></h4>
-              <h4 style='float:left;margin:0px'>
-                <small><?=$current['group']?></small><br/><small>Zuletzt geändert: <?=$current['lastChanged']?></small>
-              </h4>
-              <div style='float:right;'><input type="checkbox" id='toggle-0' data-toggle="toggle" data-on='Test' data-off='Code' data-onstyle='primary' data-offstyle='default'></div>
-              <pre class='well well-sm comment'><?=$current['comment']?></pre>
-            </div>
-          </td>
-          <? foreach($current['langs'] as $locale => $content): ?>
-            <td>
-              <div class='limit'>
-          	   <textarea class='<?=$locale?>'><?=$content?></textarea>    
-               <div class='preview <?=$locale?>'></div>
-             </div>
-            </td>
-          <? endforeach; ?>
-        </tr>
-      <? endforeach; ?>
-      <tr class='info'>
-        <td></td>
-        <? foreach($entries[0]['langs'] as $locale => $_): ?>
-          <td>
-            <div class="progress">
-              <div class="progress-bar <?=$locale?>" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:0%; min-width: 2em;">
-              </div>
-            </div>
-          </td>
-        <? endforeach; ?>
-      </tr>
-    </table>
-    <div class='text-center'>
-      <input type="checkbox" id='toggleAll' data-toggle="toggle" data-on='Alle Tests' data-off='Alle Tests' data-onstyle='primary' data-offstyle='default' data-size="large">
-      <a class='btn btn-primary btn-lg' style='width:127px' href='#'>Speichern</a><br/>
-      <nav>
-        <ul class="pagination">
+  <table class='table table-bordered' style="table-layout:fixed;position:fixed;top:0px;z-index:101"><thead>
+    <tr class='info'>
+      <th>
+        <!-- <ul class="pagination pagination-sm">
           <li>
             <a href="#" aria-label="Previous">
               <span aria-hidden="true">&laquo;</span>
@@ -194,8 +192,60 @@
               <span aria-hidden="true">&raquo;</span>
             </a>
           </li>
-        </ul>
-      </nav>
+        </ul> -->
+        <div style='float:left;'>
+          <button class='btn btn-primary'><i class="fa fa-save"></i> Speichern</button>
+        </div>
+        <div style='float:right;'>
+          <input type="checkbox" id='toggleAll' data-on-class="btn-primary" data-off-class="btn-primary" data-on-label='Test' data-off-label='Code'>
+        </div>
+      </th>
+      <? foreach($entries[0]['langs'] as $locale => $_): ?>
+        <th class='text-center'>
+          <span class='label label-default' style='float:left; margin-right: 1em; padding:5px;'>
+            <?=strtoupper($locale)?>
+          </span>
+          <div class="progress">
+            <div class="progress-bar progress-bar-warning <?=$locale?>" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:0%; min-width: 2em;">
+          </div>
+        </th>
+      <? endforeach; ?>
+    </tr>
+    </thead>
+  </table>
+  <div style='height:100%;margin-top:53px;'>
+    <table class='table table-bordered' style="table-layout:fixed"><tbody>
+      <? foreach($entries as $index => $current): ?>
+        <tr class='<?=$index?>'>
+          <td>
+            <div class='limit'>
+              <h4 style='overflow:hidden;text-overflow: ellipsis;' name='<?=$current["id"]?>'><?=$current['id']?></h4>
+              <h4 style='float:left;margin:0px'>
+                <small><?=$current['group']?></small>
+              </h4>
+              <div style='float:right;'>
+<!--                 <button class='btn btn-default'><i class="fa fa-trash"></i> Löschen</button>
+                <button class='btn btn-default'><i class="fa fa-check"></i> Geprüft</button>
+ -->                <input type="checkbox" data-on-class="btn-primary" data-off-class="btn-primary" data-on-label='Test' data-off-label='Code'>
+              </div>
+              <pre class='well well-sm comment'><?=($current["comment"] == null || $current["comment"] == "") ? "-\n" : $current["comment"]?></pre>
+            </div>
+          </td>
+          <? foreach($current['langs'] as $locale => $content): ?>
+            <td>
+              <div class='limit'>
+          	   <textarea class='<?=$locale?>'><?=$content['text'] ? $content['text'] : ""?></textarea>    
+               <div class='preview <?=$locale?>'></div>
+               <? if ($content['lastAuthor'] != null): ?>
+                 <i class="fa fa-info-circle" title="<?=$content['lastAuthor']?><br/><?=$content['lastChanged']?>" data-toggle="tooltip" data-placement="right"  style='position:absolute; bottom:10px; left: 8px;z-index:100;color:gray'></i>
+               <? endif; ?>
+             </div>
+            </td>
+          <? endforeach; ?>
+        </tr>
+      <? endforeach; ?>
+    </tbody></table>
+  </div>
 	<script type="text/javascript">
     var locales = ['de','en','fr'];
     var editorInstances;
@@ -208,15 +258,23 @@
           lineNumbers: true,
           styleActiveLine: true,
           lint: true,
-          gutters: ["CodeMirror-linenumbers", "CodeMirror-lint-markers"],
           lineWrapping: true,
-          viewportMargin: Infinity
+          viewportMargin: Infinity,
+          autoCloseBrackets: true,
+          matchBrackets: true
         });
       })
 
-      $("[type=checkbox]").not(':last').change(function(){
+      $(function () {
+        $('[data-toggle="tooltip"]').tooltip({html: true,container: "body"})
+      })
+      $(':checkbox').checkboxpicker();
+      $(':checkbox').focus(function(){
+        $(this).blur();
+      })
+
+      $("[type=checkbox]").not('#toggleAll').change(function(){
         var row = $(this).closest('tr')
-        console.log(this)
         var editors = row.find('.CodeMirror')
         var previews = row.find('.preview')
 
@@ -235,7 +293,7 @@
       })
 
       $('#toggleAll').change(function(){
-        $("[type=checkbox]").not(":last").bootstrapToggle(this.checked ? "on" : "off")
+        $("[type=checkbox]").not("#toggleAll").prop('checked',this.checked)
       })
 
       var renderPreview = function(from,to,comment, locale){
@@ -246,14 +304,13 @@
         // remove trailing <br/> 
         md = md.substring(0,md.length-6);
         var samples = generateSamples(locale,md,comment);
-        $(to).html("<div class='well well-sm'>"+samples.join("</div><div class='well well-sm'>")+"</div>");
+        $(to).html((samples.length == 0 || samples[0].match(/^\s*$/)) ? "" : "<div class='well well-sm'>"+samples.join("</div><div class='well well-sm'>")+"</div>");
       }
 
       function update(){
         for (var i = 0; i < locales.length; i++){
           var total = 0, finished = 0;
           for (var j = i; j < editorInstances.length; j += locales.length){
-            console.log(editorInstances[j])
             total += 1;
             // If contains text, consider done
             if (!editorInstances[j].getValue().match(/^\s*$/))
@@ -261,8 +318,11 @@
           }
           var pb = $('.progress-bar.'+locales[i]);
           pb.removeClass('progress-bar-success');
+          pb.removeClass('progress-bar-warning');
           if (total == finished)
             pb.addClass('progress-bar-success');
+          else
+            pb.addClass('progress-bar-warning');
           pb.html(finished+'/'+total);
           pb.css('width',finished/total*100+'%');
         }
